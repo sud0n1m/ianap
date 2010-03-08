f15ed9ee9d8ea5380e892fd971569ea15048a5c9
Colin Nederkoorn <colin@nederkoorn.co.uk>
2010-03-07 19:29:36 -0500

    added some files

---
+++ homepage.php
@@ -0,0 +1,71 @@
+<?php
+/*
+Template Name: DaHomepage
+*/
+?>
+<?php
+
+    // calling the header.php
+    get_header();
+
+    // action hook for placing content above #container
+    thematic_abovecontainer();
+
+?>
+
+	<div id="container">
+		<div id="content">
+
+            <?php
+            
+            the_post();
+            
+            ?>
+            
+			<div id="post-<?php the_ID(); ?>" class="<?php thematic_post_class() ?>">
+    			<?php
+                
+                // creating the post header
+                
+                ?>
+                
+				<div class="entry-content">
+                
+                    <?php
+                    
+                    the_content();
+                    
+                    ?>
+
+                    
+                    <?php
+                    
+                    edit_post_link(__('Edit', 'thematic'),'<span class="edit-link">','</span>');
+                    
+                    ?>
+
+				</div>
+			</div><!-- .post -->
+
+            <?php 
+        
+            if ( get_post_custom_values('comments') ) 
+                comments_template() // Add a key/value of "comments" to enable comments on pages!
+        
+            ?>
+
+		</div><!-- #content -->
+	</div><!-- #container -->
+
+<?php 
+
+    // action hook for placing content below #container
+    thematic_belowcontainer();
+
+    // calling the standard sidebar 
+    thematic_sidebar();
+    
+    // calling footer.php
+    get_footer();
+
+?>
